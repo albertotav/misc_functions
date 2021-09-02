7f0b31ac2c4d057d30df26360c4d289b79e65fe5
Alberto Tavares <albertotds@gmail.com>
2021-09-02 16:50:00 -0300

    Update data_cleaning.py

--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -29,3 +29,56 @@ def translate_month_pt(df):
       '(?i)Novembro':'november', '(?i)Nov':'nov',
       '(?i)Dezembro':'december', '(?i)Dez':'dec',}, regex=True)
   return df
+
+def normalize_string(string, case = None):
+    """Return string without special characters.
+    
+    UPPERCASE if case = 'upper' or 'uppercase'
+
+    lowercase if case = 'lower' or 'lowercase'
+    """
+    dict_special_char ={
+    'â':'a','ã':'a','á':'a','à':'a','ä':'a',
+    'Â':'A','Ã':'A','Á':'A','À':'A','Ä':'A',
+    'ê':'e','é':'e','è':'e','ë':'e',
+    'Ê':'E','É':'E','È':'E','Ë':'E',
+    'í':'i','ì':'i','ï':'i',
+    'Í':'I','Ì':'I','Ï':'I',
+    'ô':'o','õ':'o','ó':'o','ò':'o','ö':'o',
+    'Ô':'O','Õ':'O','Ó':'O','Ò':'O','Ö':'O',
+    'ú':'u','ù':'u','ü':'u',
+    'Ú':'U','Ù':'U','Ü':'U',
+    'ç':'c','Ç':'C',
+    '¹':'1','²':'2','³':'3',
+    'ª':'','º':'','°':'',
+    '£':'','¢':'','¬':'','§':'',
+    '^':'','´':'','`':'','~':'','¨':'',
+    ':':'_',';':'_','.':'_',',':'_','<':'_','>':'_',
+    '+':'','=':'',
+    ' ':'_','.':'','/':'_','__':'_',
+    '!':'','?':'','@':'_','$':'','%':'','#':'','&':'','*':'_',
+    '(':'_',')':'_','[':'_',']':'_','{':'_','}':'_',
+    }
+    excess_underscore ={
+        '_____': '_',
+        '____':'_',
+        '___':'_',
+        '__':'_',
+    }
+    # Remove special characters
+    for key in dict_special_char.keys():
+        string = string.replace(key, str(dict_special_char[key]))
+    # Remove excessive underscore
+    for key in excess_underscore.keys():
+        string = string.replace(key, str(excess_underscore[key]))
+    # Remove underscore from start/end of string
+    if string[0] == '_':
+        string = string[1:]
+    if string[-1] == '_':
+        string = string[:-1]
+    # Set upper or lowercase
+    if case == 'upper' or case == 'uppercase':
+        return string.upper()
+    if case == 'lower' or case == 'lowercase':
+        return string.lower()
+    return string
